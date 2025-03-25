ba05059bf118fbd811466626302e681f0646605c
Foxykwhite <101445839+Foxykwhite@users.noreply.github.com>
2025-03-25 18:48:27 +0000

    new2

--- djangotutorial/polls/apps.py
+++ djangotutorial/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangostartappConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "djangostartapp"
+    name = "polls"
--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -2,12 +2,44 @@ from django.db import models
 
 # Create your models here.
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+# Учёт заказов в интернет-магазине
 
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    date_registered = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('completed', 'Completed'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} (Order {self.order.id})"
