9de73c2ec226d9ed09845fcaf62965cfe430804b
Foxykwhite <101445839+Foxykwhite@users.noreply.github.com>
2025-03-27 19:09:58 +0000

    docs

---
+++ docs/myapp.rst
@@ -0,0 +1,5 @@
+.. automodule:: mysite.models
+   :members:
+
+.. automodule:: mysite.views
+   :members:
---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../djangotutorial'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+import django
+django.setup()
+     
+project = 'djangotutorial'
+copyright = '2025, alekseyf'
+author = 'alekseyf'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
